efa11d4e0427616464002ae6e44d99bfebe4d104
Vimal Raveendran <vimalraveendra@gmail.com>
2024-03-12 22:47:02 +0100

    fetched monsters frrom third party ,written fetchMonsters functionality, updated app & card-list components to render the monsters card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Monster } from './Model/Monsters.model';
+import { HttpClient } from '@angular/common/http';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit ,OnDestroy{
   title = 'monster-app-two-way-bind';
+   isFetching:boolean=false;
    monsters:Monster[]=[];
+   error:String =null;
+   subscription:Subscription;
 
-  constructor(){}
+  constructor(private http:HttpClient){}
 
   ngOnInit() {
-      
+      this.subscription = this.fetchMonsters();
   }
+
+
+
+   private fetchMonsters(){
+    this.isFetching=true;
+     return this.http.get("https://jsonplaceholder.typicode.com/users")
+     .subscribe({
+      next:(users:Monster[])=>{
+        console.log("users",users)
+        this.isFetching =false;
+        this.monsters=users;
+      },
+      error:(error:any)=>{
+        this.isFetching =false;
+        this.error=error.message;
+      }
+     })
+   }
+
+   ngOnDestroy(): void {
+      this.subscription.unsubscribe();
+   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { CardListComponent } from './card-list/card-list.component';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +13,9 @@ import { CardListComponent } from './card-list/card-list.component';
     CardListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
