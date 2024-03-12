17c6308cb54fd6f47a225b074c6f81b486bcf0b2
Vimal Raveendran <vimalraveendra@gmail.com>
2024-03-12 22:27:27 +0100

    added model & card-list component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Monster } from './Model/Monsters.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'monster-app-two-way-bind';
+   monsters:Monster[]=[];
+
+  constructor(){}
+
+  ngOnInit() {
+      
+  }
 }
